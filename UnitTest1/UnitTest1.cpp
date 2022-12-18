d09c824feba111d7ef24bdd8499db1dd17a97fc3
krabster <bohdan.kzymyr.ik.2022@lpnu.ua>
2022-12-18 10:17:22 +0200

    f

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Laba 11.2/Laba 11.2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char* fname;
+			fname = 3;
+			int N = 3;
+			Student* p = new Student[N];
+			Create(p, fname, N);
+			Print(p, N);
+			LineSearch(p, N);
+		}
+	};
+}
